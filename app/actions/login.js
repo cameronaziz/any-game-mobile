dcb53c443cce8d82b70b5105759abc0741074c1a
cameronaziz <cameronaziz@gmail.com>
2017-04-26 14:16:55 -0700

    clean up

--- app/actions/login.js
+++ app/actions/login.js
@@ -3,32 +3,31 @@ import * as firebase from '../lib/firebase';
 import * as loading from './loading';
 import { AsyncStorage } from 'react-native';
 
-
 export function loginUser(email, password) {
-  return(dispatch, getState) => {
-    let currentlyLoading = true;
-    dispatch(loading.setLoading( { currentlyLoading }));
-    console.log('Logging in user ' + email);
-    firebase.auth.signInWithEmailAndPassword(email, password)
-        .then((user) => {
-          dispatch(setAuthenticatedUser({user}));
-          AsyncStorage.setItem('USER', JSON.stringify(user));
-          firebase.db.ref('/users/' + user.uid ).on('value', function (snapshot) {
-            AsyncStorage.setItem('USER_DATA', JSON.stringify(snapshot.val()));
-          },function (error) {
-            console.log(error)
-          });
-          let loginSuccess = true;
-          dispatch(setLoginSuccess({ loginSuccess }));
-          currentlyLoading = false;
-          dispatch(loading.setLoading({ currentlyLoading }));
-        })
-        .catch((error) => {
-          dispatch(setLoginError( error ));
-          currentlyLoading = false;
-          dispatch(loading.setLoading({ currentlyLoading }));
-    });
-  }
+    return (dispatch, getState) => {
+        let currentlyLoading = true;
+        dispatch(loading.setLoading({ currentlyLoading }));
+        console.log('Logging in user ' + email);
+        firebase.auth.signInWithEmailAndPassword(email, password)
+            .then((user) => {
+                dispatch(setAuthenticatedUser({ user }));
+                AsyncStorage.setItem('USER', JSON.stringify(user));
+                firebase.db.ref('/users/' + user.uid).on('value', function(snapshot) {
+                    AsyncStorage.setItem('USER_DATA', JSON.stringify(snapshot.val()));
+                }, function(error) {
+                    console.log(error)
+                });
+                let loginSuccess = true;
+                dispatch(setLoginSuccess({ loginSuccess }));
+                currentlyLoading = false;
+                dispatch(loading.setLoading({ currentlyLoading }));
+            })
+            .catch((error) => {
+                dispatch(setLoginError(error));
+                currentlyLoading = false;
+                dispatch(loading.setLoading({ currentlyLoading }));
+            });
+    }
 }
 
 export function setAuthenticatedUser({ user }) {
@@ -38,16 +37,12 @@ export function setAuthenticatedUser({ user }) {
   }
 }
 
-
-
 export function setLoginSuccess( success ){
   return {
     type: types.LOGIN_SUCCESS,
     success
   }
 }
-
-
 
 export function setLoginError( error ) {
   return {
